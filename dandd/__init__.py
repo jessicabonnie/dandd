fab434d79ee2447c55dfda8722468053b182e2d1
Jessica Bonnie <1863770+jessicabonnie@users.noreply.github.com>
2025-02-14 12:11:05 -0500

    file reorg

---
+++ dandd/__init__.py
@@ -0,0 +1,9 @@
+"""
+DandD: Efficient measurement of sequence growth and similarity
+
+A tool to estimate deltas for sequence sets and answer questions about relative contribution.
+"""
+
+from .core import DandD
+
+__version__ = "1.0.0"
